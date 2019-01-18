f06f2b010d8445a732f44b8603d7832b7e2c5930
macanel <macanel@yandex.ru>
2019-01-18 11:29:47 +0300

    correct step 4-2

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ const hello = (description) => {
   }
   return '';
 };
-export const userName = () => {
+const userName = () => {
   const putName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${putName}!`);
+  console.log('');
   return putName;
 };
 
@@ -21,33 +22,12 @@ const getRandomNumber = (min = 0, max = 100) => Math.floor(Math.random() * (max 
 const isEven = number => number % 2 === 0;
 
 export const even = () => {
-  console.log(hello('Welcome to the Brain Games!'));
-  console.log('Answer "yes" if number even otherwise answer "no".');
+  console.log(hello('Answer "yes" if number even otherwise answer "no".'));
   const usName = userName();
   for (let i = 1; i <= maxNumberOfAnswers; i += 1) {
     const randomNumber = getRandomNumber();
     console.log(`Question: ${randomNumber}`);
     const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
-    const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer !== correctAnswer) {
-      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}"`);
-      console.log(`Let's try again, ${usName}!`);
-      return;
-    }
-    console.log('Correct!');
-  }
-  console.log(`Congratulations, ${usName}!`);
-};
-
-export const gameTemplate = (task, description) => {
-  console.log(hello(description));
-  const usName = userName();
-  console.log(`Hello, ${usName}!`);
-  for (let i = 1; i <= maxNumberOfAnswers; i += 1) {
-    const gameConditions = task();
-    const taskString = car(gameConditions);
-    console.log(`Question: ${taskString}`);
-    const correctAnswer = String(cdr(gameConditions));
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer !== correctAnswer) {
       console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}"`);
