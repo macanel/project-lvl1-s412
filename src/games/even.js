65308bd1565bdf35e53a0e81983a0c21aa4c3ef2
macanel <32230828+macanel@users.noreply.github.com>
2019-01-19 10:26:02 +0300

    Update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -6,8 +6,8 @@ const isEven = number => number % 2 === 0;
 const taskDescription = 'Answer "yes" if number even otherwise answer "no".';
 
 const even = () => {
-  const randomNumber = getRandomNumber(0, 100);
-  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
-  return cons(randomNumber, correctAnswer);
+  const question = getRandomNumber(0, 100);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return cons(question, correctAnswer);
 };
 export default () => gameTemplate(even, taskDescription);
